c625f68b8740993060fa725e3920f2b84f41da78
JEAR2 <jhedacro@gmail.com>
2022-07-09 19:44:05 -0500

    parte05Merge terminada

--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 /**
  * Learn how to merge flux.
  *
@@ -15,21 +17,25 @@ public class Part05Merge {
 
 	// TODO Merge flux1 and flux2 values with interleave
 	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		Flux<User> result = Flux.merge(flux1.delayElements(Duration.ofMillis(300)),flux2.delayElements(Duration.ofMillis(100)));
+		return result;
+
 	}
 
 //========================================================================================
 
 	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
 	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		Flux<User> result = flux1.concatWith(flux2);
+		return result;
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux containing the value of mono1 then the value of mono2
 	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		Flux<User> result = Flux.concat(mono1).concatWith(mono2);
+		return result;
 	}
 
 }
